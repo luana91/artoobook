7fb8a0e849c3c276385a333643cc96ae37ceba1b
LUNA <lulypoggio@gmail.com>
2017-03-08 16:05:39 +0100

    modifiche

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ var app = express();
 const PORT = 3000;
 
 
-//bootstrap
-app.use('/bootstrap', express.static(path.join(__dirname, "..", "node_modules", "bootstrap", "dist")))
-app.use('/jquery', express.static(path.join(__dirname, "..", "node_modules", "jquery", "dist")))
+//bootstrap e jquery
+app.use('/bootstrap', express.static(path.join(__dirname, "..", "node_modules", "bootstrap", "dist")));
+app.use('/jquery', express.static(path.join(__dirname, "..", "node_modules", "jquery", "dist")));
+
+//css e js
+app.use('/css', express.static(path.join(__dirname, "..", "client", "css")));
+app.use('/js', express.static(path.join(__dirname, "..", "client", "js")));
 
 //serviamo la index
 app.get('/',function(req,res){
