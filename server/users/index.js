d0acd3a4769965830f11147a2eb6715b25aabfca
LUNA <lulypoggio@gmail.com>
2017-03-08 18:03:11 +0100

    modifiche

--- server/users/index.js
+++ server/users/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var Users = require('./users.controller.js')
+var Users = require('./users.controller.js');
 
+//LISTA UTENTI
 router.get('/', Users.getUsers);
 
+//CREAZIONE UTENTE
+router.post('/', Users.createUsers);
+
+//DETTAGLIO UTENTE
+router.get('/:id', Users.detailUsers);
+
+//ELIMINAUTENTE
+router.delete('/:id', Users.deleteUsers);
+
+//AGGIORNA
+router.put('/:id', Users.updateUsers);
 
 module.exports = router;
--- server/users/users.controller.js
+++ server/users/users.controller.js
@@ -1,8 +1,33 @@
+
 module.exports = (function(){
     var getUsers = function(req, res) {
         res.send("lista utenti");
     };
+
+    var createUsers = function(req, res) {
+        res.send("creazioni utenti");
+    };
+
+    var detailUsers = function(req, res) {
+        var id = req.params.id;
+        res.send("dettaglio utente con id:" + id);
+    };
+
+    var deleteUsers = function(req, res) {
+        var id = req.params.id;
+        res.send("eliminato utente con id:" + id);
+    };
+
+    var updateUsers = function(req, res) {
+        var id = req.params.id;
+        res.send("aggiornato utente con id:" + id);
+    };
+
     return {
-        getUsers: getUsers
+        getUsers: getUsers,
+        createUsers: createUsers,
+        detailUsers: detailUsers,
+        deleteUsers: deleteUsers,
+        updateUsers: updateUsers,
     }
 })();
