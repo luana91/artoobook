4c119e148d5ab4dc0db750fe40eda6e24b73478a
LUNA <lulypoggio@gmail.com>
2017-03-09 15:32:02 +0100

    tabella

--- client/js/script.js
+++ client/js/script.js
@@ -13,8 +13,28 @@ $(document).ready(function(){
         }
        Users.creaUtente(nuovo);
        this.reset();
+       disegna();
     });
-    Users.getUsers().then(function(data){
-        console.log(data);
+
+
+    function disegna(){
+        var str= "";
+
+    Users.getUsers().then(function(users){
+       for(let i=0; i<users.length; i++ ){
+           str +="<tr>" +
+           "<td>" + users[i].nome + "</td>" +
+           "<td>" + users[i].cognome + "</td>" +
+           "<td>" + users[i].sesso + "</td>" +
+           "<td>" + users[i].età + "</td>" +
+           "<td><span idutente='"+ users[i]._id +"' class='glyphicon glyphicon-trash' style='cursor : pointer'></span></td>" +
+           "<td><span idmodifica='"+ users[i]._id +"' class='glyphicon glyphicon-pencil' style='cursor : pointer'></span></td>" +
+           "</tr>";
+       }
+
+       $("#tbody").html(str);
     }).catch();
+};
+
+disegna();
 });
